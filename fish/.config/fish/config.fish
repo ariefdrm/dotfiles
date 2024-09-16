1b66dd73974a1687b0666760576594c23ef100d0
ariefdrm <ariefdarmawan5522@gmail.com>
2024-09-16 22:37:26 +0700

    add .tmux/ and changes fish config

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,7 +1,23 @@
 # set alias 
 alias fishconfig="cd ~/.config/fish"
 alias nvimconfig="cd ~/.config/nvim"
-alias gc="git commit -m"
+alias dotfiles="cd ~/.dotfiles"
+
+# set alias for git 
+alias gcm="git commit -m"
+alias gsw="git switch"
+alias gad="git add"
+alias gadd="git add ."
+alias glog1="git log --oneline"
+alias glog="git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
+alias gp="git push"
+alias gco="git checkout"
+alias gst="git status"
+
+# set alias for exa ls replacement
+alias ls='exa --icons'
+alias ll='exa --icons -l'
+alias lt='exa --icons --tree'
 
 # export path
 set -x LANGUAGE en_US.UTF-8
