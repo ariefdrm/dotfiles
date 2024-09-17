97c80894ab124e90cea7c1a7aad4196a4da9db01
ariefdrm <ariefdarmawan5522@gmail.com>
2024-09-17 21:16:57 +0700

    change fish config
    changes fish config add alias used for fzf

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,18 +1,18 @@
 # set alias 
-alias fishconfig="cd ~/.config/fish"
-alias nvimconfig="cd ~/.config/nvim"
+alias fishconfig='cd ~/.config/fish'
+alias nvimconfig='cd ~/.config/nvim'
 alias dotfiles="cd ~/.dotfiles"
 
 # set alias for git 
-alias gcm="git commit -m"
-alias gsw="git switch"
-alias gad="git add"
-alias gadd="git add ."
-alias glog1="git log --oneline"
-alias glog="git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
-alias gp="git push"
-alias gco="git checkout"
-alias gst="git status"
+alias gcm='git commit -m'
+alias gsw='git switch'
+alias gad='git add'
+alias gadd='git add .'
+alias glog1='git log --oneline'
+alias glog='git log --graph --pretty=format:"%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset" --abbrev-commit'
+alias gp='git push'
+alias gco='git checkout'
+alias gst='git status'
 
 # set alias for exa ls replacement
 alias ls='exa --icons'
@@ -25,8 +25,12 @@ set -x LC_CTYPE en_US.UTF-8
 set -x LC_ALL en_US.UTF-8
 set -x LANG en_US.UTF-8
 
+# alias fzf used to open multiple files with neovim
+alias nvi='nvim $(fzf -m --preview="bat --color=always {}")'
 
 if status is-interactive
     # Commands to run in interactive sessions can go here
-  oh-my-posh init fish --config "~/.config/fish/catppuccin.omp.json" | source
+oh-my-posh init fish --config "~/.config/fish/catppuccin.omp.json" | source
+
+fzf --fish | source
 end
